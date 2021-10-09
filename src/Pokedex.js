f9c2296553704728234452c104170cd4e3647782
Edson Lima <edsoneduardo526@gmail.com>
2021-10-09 13:40:44 -0300

    Adiciona funcionalidade de trocar o pokémon

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import Pokemon from './Pokemon';
+import data from './data.js'
 
 class Pokedex extends React.Component {
+
+    constructor() {
+        super()
+            this.state = {
+                pokemon: 0
+            }
+            this.nextPokemon = this.nextPokemon.bind(this)
+    }
+
+    nextPokemon = () => {
+        if (this.state.pokemon < (data.length - 1)) {
+            this.setState((pokemonAnterior, _props) => ({
+                pokemon: pokemonAnterior.pokemon + 1
+            }))
+        } else {
+            this.setState(() => ({
+                pokemon: 0
+            }))
+        }
+    }
+
     render() {
         return (
+            <div>
+            <button onClick={this.nextPokemon}>Próximo pokémon</button>
             <div className="pokedex">
-                {this.props.pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon} />)}
+              <Pokemon key={data[this.state.pokemon].id} pokemon={data[this.state.pokemon]} />
+            </div>
             </div>
         );
     }
